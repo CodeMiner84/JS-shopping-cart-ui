38e0de4e282a60b6f1df40697b0035180d7a6552
MichałPietrasz <michal.pietrasz@gmail.com>
2018-12-04 22:42:18 +0100

    initialize Dashboard / Product / Cart routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
+import Product from './components/Product/index';
+import Cart from './components/Cart';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        test
+        <Router>
+          <React.Fragment>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/product">Product</Link>
+                  </li>
+                  <li>
+                    <Link to="/cart">Cart</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div>SIDEBAR</div>
+            <Route path="/" exact={true} component={Dashboard} />
+            <Route path="/product" exact={true} component={Product} />
+            <Route path="/cart" exact={true} component={Cart} />
+          </React.Fragment>
+        </Router>
+        {/* ROUTER */}
+        {/* LINK: / */}
+        {/* LINK: product/:id */}
+        {/* LINK: cart */}
       </div>
     );
   }
---
+++ src/components/Product/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default class Product extends React.Component<{}, {}> {
+  render() {
+    return <div>Product page</div>;
+  }
+}
