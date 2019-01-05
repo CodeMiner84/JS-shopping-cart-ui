2d28c0b05b55ddfc63ad41084839469e0dfac319
michal <michal.pietrasz@gmail.com>
2019-01-05 17:03:23 +0100

    Fix cart model

--- src/models/cart.tsx
+++ src/models/cart.tsx
@@ -1,10 +1,10 @@
-export type CartItem = {
+export interface CartItem {
   _id: string;
   title: string;
   price: number;
   image: string;
   quantity: number;
   product_id?: number;
-};
+}
 
 export type Cart = CartItem[];
